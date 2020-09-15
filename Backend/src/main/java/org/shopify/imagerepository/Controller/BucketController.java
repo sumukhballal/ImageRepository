849fb75accb5454f6dac8c95330230be5fcc4fc5
Sumukh Ballal <sumukhballal@gmail.com>
2020-09-15 01:11:11 -0500

    Fix : Fixed Server not sending JSON response

--- Backend/src/main/java/org/shopify/imagerepository/Controller/BucketController.java
+++ Backend/src/main/java/org/shopify/imagerepository/Controller/BucketController.java
@@ -6,6 +6,9 @@ import org.shopify.imagerepository.Service.DatabaseClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class BucketController {
 
         private static final Logger logger = LoggerFactory.getLogger(BucketController.class);
+        private String message;
 
         @Autowired
         private AmazonClient amazonClient;
@@ -25,18 +29,22 @@ public class BucketController {
         BucketController(AmazonClient amazonClient)
         {
             this.amazonClient=amazonClient;
+            this.message="";
         }
 
-        @PostMapping("/upload")
-        public  String uploadFile(@RequestPart(value="file") MultipartFile file,
-                                  @RequestPart(value="name",required = false) String name) {
+        @PostMapping(value = "/upload",produces = MediaType.APPLICATION_JSON_VALUE)
+        public String uploadFile(@RequestPart(value="file") MultipartFile file,
+                                                 @RequestPart(value="name",required = false) String name) {
             String currName=(name==null)?file.getOriginalFilename():name;
             logger.info("Upload file "+name);
             String url = this.amazonClient.uploadFile(file);
             logger.info("Uploaded to S3 Bucket "+url);
             /* Store image details in MYSQL */
             this.databaseClient.store(currName,file.getContentType(),url);
+            this.message="{\n" +
+                    "\t\"text\": \"Image uploaded successfully!\"\n" +
+                    "}";
 
-            return "Upload Image successfully!";
+            return this.message;
         }
 }
